3cb445f1b323726cb80c60b2343b679b751c616e
Dmitry Kostarev <albemuth1221@gmail.com>
2016-05-14 21:26:09 +0500

    add deploy script and Web.config template

---
+++ deploy.ps1
@@ -0,0 +1,17 @@
+﻿$sourcePath = 'ContosoUniversity\'
+$destination = '.deploy'
+
+$foldersToCopy = @('bin', 'Content', 'fonts', 'Scripts', 'Views')
+$filesToCopy = @('favicon.ico', 'Global.asax', 'packages.config', 'Web.config.template')
+
+if(Test-Path $destination){
+    Remove-Item $destination -Recurse
+}
+New-Item -Name $destination -ItemType Directory
+
+foreach($folder in $foldersToCopy){
+    Copy-Item ($sourcePath + $folder) ($destination) -Recurse
+}
+foreach($file in $filesToCopy){
+    Copy-Item ($sourcePath + $file) ($destination+ '\' +$file -replace '\.template')
+}
